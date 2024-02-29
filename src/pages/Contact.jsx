27392d9ddfc33481a38fe4f02b4b47a6cd3aefa6
Andersson Cordoba <anderson.980114@gmail.com>
2024-02-29 12:08:32 -0500

    deploy settings

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'styled-components';
+import styled from 'styled-components'; 
 
 const Contact = () => {
     return (
